efef24c129587143a5e8f3b4df22050803bb7a75
Philipp Ines <93800283+N01498982@users.noreply.github.com>
2022-04-03 22:31:33 -0400

    For Lab 08
    Never mind, I was able to update the file by removing the duplicated file within the repository.

---
+++ student-list_Philipp_Ines/student-list_Philipp_Ines/src/StudentPosts/StudentPosts.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+
+export const StudentPosts = ({ isAdmin }) => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setPosts(json))
+      .catch((error) => console.log(error));
+    return () => {
+      "Cleanup codes";
+    };
+  }, []);
+
+  if (!isAdmin) {
+    return null;
+  }
+  return (
+    <>
+      <h2>What students are thinking</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Titles</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.slice(0, 10).map(({ index, title, body }) => (
+            <tr key={index}>
+              <td>{title}</td>
+              <td>{body}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
